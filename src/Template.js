22090daa58a531d155d5fa7d0c09cb04b1ed4631
Adil Zhakypbek <32118989+azhakypb@users.noreply.github.com>
2019-09-28 14:57:26 -0400

    template is ready

--- src/Template.js
+++ src/Template.js
@@ -2,21 +2,27 @@ import React from 'react';
 //import './template.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap'
+import './Template.css';
 
 function Template() {
   return (
     <div className="row">
-      <div className="column" style="width:33%; padding-left: 1%; position: sticky; top: 0;">
-        One of three columns
-        <br>
-        <button className="btn" ><i class="fas fa-user-cog">Settings</i></button>
-        <br>
-        <button className="btn" ><i class="fas fa-user">My Profile</i></button>
-      </div>
-      <div className="column" style="width:47%">
+      <div className="column" id="firstCol">
+        <br />
+        <Button variant="primary">My Profile</Button>
+        <br />
+        <br />
+        <Button variant="primary">Settings</Button>
+        <br />
+        <br />
+        <Button variant="primary">Log out</Button>
+        <br />
+        </div>
+      <div className="column" id="secCol">
         One if three columns
       </div>
-      <div className="column" style="width:20%">
+      <div className="column" id="thirdCol">
         One of three columns
       </div>
     </div>
