561d5b17c8a400385f62b66e989fb2de64edc86e
Adil Zhakypbek <32118989+azhakypb@users.noreply.github.com>
2019-10-31 14:57:20 -0400

    update

--- src/pages/OtherProfile.js
+++ src/pages/OtherProfile.js
@@ -5,7 +5,6 @@ import {Button, Card, Col, Container, Jumbotron, Row, Image} from 'react-bootstr
 import { Auth } from 'aws-amplify';
 // components
 import Navbar from '../components/Navbar.js'
-import FollowList from '../components/FollowList.js'
 import DBOps from '../DBOps.js'
 import awsmobile from '../aws-exports.js'
 var AWS = require('aws-sdk');
@@ -19,7 +18,7 @@ class OtherProfile extends Component {
             name        : '',
             username    : '',
             url         : 'https://vyshnevyi-partners.com/wp-content/uploads/2016/12/no-avatar-300x300.png',
-            me          : ''       
+            me          : ''
         }
         // bind functions
         this.follow = this.follow.bind(this);
@@ -45,18 +44,18 @@ class OtherProfile extends Component {
 
                 console.log(err, err.stack); // an error occurred
             } else{
-                
+
                 this.setState({ username: window.location.href.split('/').slice(-1)[0] });
 
                 for(var i = 0; i < data.UserAttributes.length; i++ ){
-                    if( data.UserAttributes[i].Name === 'name'){ 
+                    if( data.UserAttributes[i].Name === 'name'){
                         this.setState({name:data.UserAttributes[i].Value})
                     }
-                    else if(data.UserAttributes[i].Name === 'picture'){ 
+                    else if(data.UserAttributes[i].Name === 'picture'){
                         this.setState({url:data.UserAttributes[i].Value})
                     }
                 }
-            } 
+            }
         });
     }
 
@@ -127,7 +126,7 @@ class OtherProfile extends Component {
             </Col>
 
             <Col>
-                <FollowList username={username}></FollowList>
+                <h1>.</h1>
             </Col>
       </Row>
     );
