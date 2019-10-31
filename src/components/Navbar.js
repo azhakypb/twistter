ef0907757118f3f5f8be963ab9c13da00cc65b87
Jihyok Woo <woo18@purdue.edu>
2019-10-31 13:18:30 -0400

    Delete user prompt message added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 // react modules
 import React, { Component, } from 'react';
-import { Button, Jumbotron } from 'react-bootstrap';
+import { Button, Jumbotron, Modal } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 // aws modules
 import { Auth } from 'aws-amplify';
@@ -12,6 +12,11 @@ class Navbar extends Component {
 	constructor(props){
 		// props and states
     	super(props);
+	this.state = {
+	    visibleDeletePrompt	: false,
+	    visibleDeleteNo	: false,
+	    visibleDeleteYes	: false
+	};
     	// bind functions
 	  //  this.displayUserAttributes 	=	this.displayUserAttributes	.bind(this);
 	    this.deleteUser				=	this.deleteUser.bind(this);
@@ -45,9 +50,65 @@ class Navbar extends Component {
             .catch(this.onError);
   	}
 
+	// Modal display control functions  
+	showDeletePrompt() { 
+	  this.setState({ visibleDeletePrompt: true }); 
+	}
+
+  	closeDeletePromptNo() {
+    	  this.setState({
+      	    visibleDeletePrompt: false,
+      	    visibleDeleteNo: true
+    	  });
+  	}
+
+  	closeDeletePromptYes() {
+	  this.deleteUser();
+    	  this.setState({
+      	    visibleDeletePrompt: false,
+      	    visibleDeleteYes: true
+    	  });
+  	}
+
+  	closeDeleteNo() {
+    	  this.setState({ visibleDeleteNo: false });
+  	}
+
+  	closeDeleteYes() {
+    	  this.setState({ visibleDeleteYes: false });
+  	}
+
+
 	render() {
 
     	return(
+
+	    <div>
+		<Modal show={this.state.visibleDeletePrompt} onHide={this.closeDeletePromptNo.bind(this)}>
+		  <Modal.Header></Modal.Header>
+		  <Modal.Body>Are you sure you want to continue? All of your information will be deleted.</Modal.Body>
+      		  <Modal.Footer>
+        		<Button variant="secondary" onClick={this.closeDeletePromptNo.bind(this)}>No</Button>
+        		<Button variant="primary" onClick={this.closeDeletePromptYes.bind(this)}>Yes</Button>
+      		  </Modal.Footer>
+    		</Modal>
+ 
+    		<Modal show={this.state.visibleDeleteNo} onHide={this.closeDeleteNo.bind(this)}>
+      		  <Modal.Header></Modal.Header>
+      		  <Modal.Body>Cancelled.</Modal.Body>
+      		  <Modal.Footer>
+			<Button variant="secondary" onClick={this.closeDeleteNo.bind(this)}>Close</Button>
+      		  </Modal.Footer>
+    		</Modal>
+
+    		<Modal show={this.state.visibleDeleteYes} onHide={this.closeDeleteYes.bind(this)}>
+      		  <Modal.Header></Modal.Header>
+      		  <Modal.Body>Account successfully deleted.</Modal.Body>
+      		  <Modal.Footer>
+	  		<Button variant="secondary" onClick={this.closeDeleteYes.bind(this)}>Close</Button>
+      		  </Modal.Footer>
+    		</Modal>
+
     		<Jumbotron>
     			<h2>Navbar</h2>
     			<Link
@@ -90,11 +151,13 @@ class Navbar extends Component {
   					<Button
 						variant="secondary"
 						size="md"
-						onClick ={this.deleteUser}
+						onClick ={this.showDeletePrompt.bind(this)}
 						block>
 						Delete User
 					</Button>
     		</Jumbotron>
+	    </div>
+
     	);
   	}
 }
