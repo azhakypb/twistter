fd2ba53b941202d7606db66be4a06544d3cada8b
Anthony Michael Root <root9@data.cs.purdue.edu>
2019-10-31 14:56:06 -0400

    Current dev with followlist

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,7 +5,7 @@ import { Card, Col, Container,  Jumbotron, Row, Image } from 'react-bootstrap';
 import { Auth } from 'aws-amplify';
 // components
 import Navbar from '../components/Navbar.js'
-
+import FollowList from '../components/FollowList.js'
 class Profile extends Component {
 
     constructor(props){
@@ -66,6 +66,7 @@ class Profile extends Component {
             </Col>
 
             <Col>
+            <FollowList username={username}></FollowList>
             </Col>
       </Row>
     );
