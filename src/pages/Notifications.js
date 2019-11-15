70fec7da3af90013e9dd2d4bec6628dfe3de663a
Anthony Michael Root <root9@data.cs.purdue.edu>
2019-11-15 09:31:58 -0500

    Formatting changes

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -6,50 +6,56 @@ import Navbar from '../components/Navbar.js'
 
 class Notifications extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: "",
-      notifications: []
-    }
-  }
+  	constructor(props) {
+    	super(props);
+    	this.state = {
+      		user: "",
+      		notifications: []
+    	}
+  	}
 
-  async componentDidMount(){
-    var user = await Auth.currentAuthenticatedUser({ bypassCache: true });
-    this.setState({user: user.username});
-    var userData = await new DBOps().searchUser(JSON.stringify({id: this.state.user}));
-    this.setState({notifications: userData.getUser.notifications.items})
-    console.log(this.state.notifications);
+  	async componentDidMount(){
+    	var user = await Auth.currentAuthenticatedUser({ bypassCache: true });
+    	this.setState({user: user.username});
+    	var userData = await new DBOps().searchUser(JSON.stringify({id: this.state.user}));
+    	this.setState({notifications: userData.getUser.notifications.items})
+    	console.log(this.state.notifications);
 
-	for(var i = 0; i < this.state.notifications.length; i++){
-		var deleteID = this.state.notifications[i].id;
-		await new DBOps().deleteNotification(JSON.stringify({id: deleteID}));
-	}
-  }
+		for(var i = 0; i < this.state.notifications.length; i++){
+			var deleteID = this.state.notifications[i].id;
+			await new DBOps().deleteNotification(JSON.stringify({id: deleteID}));
+		}
+  	}
 
-  render() {
-    var temp = [];
+  	render() {
+    	var temp = [];
 
-    for (const [index, value] of this.state.notifications.entries()) {
-      temp.push(<div>{value.text}</div>);
-    }
+    	for (const [index, value] of this.state.notifications.entries()) {
+      		temp.push(value.text);
+    	}
 
-    return (
-		<div>
-			<Row>
-				<Col>
-					<Navbar></Navbar>
-				</Col>
-				<Col md="6" xs="10" >
-      				{temp.map(item => (
-						<Alert key={item} variant='primary'><p>{item}</p>
-						</Alert>))}
-				</Col>
-				<Col>
-				</Col>
-			</Row>
-    	</div>
-	);
+		if(temp.length === 0){
+			temp.push("You have no new notifications.");
+		}
+
+    	return (
+			<div>
+				<Row>
+					<Col>
+						<Navbar></Navbar>
+					</Col>
+					<Col md="6" xs="10" >
+						<br />
+      					{temp.map(item => (
+							<Alert key={item} variant='info'>
+							{item}
+							</Alert>))}
+					</Col>
+					<Col>
+					</Col>
+				</Row>
+    		</div>
+		);
 	}
 
 }
