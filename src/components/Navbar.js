bea458b1e30783182e179d34fdb56906aa526490
Anthony Michael Root <root9@data.cs.purdue.edu>
2019-11-21 19:52:10 -0500

    Confirmation for delete account

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 // react modules
 import React, { Component, } from 'react';
-import { Button, Jumbotron } from 'react-bootstrap';
+import { Button, Jumbotron, Modal } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 // aws modules
 import { Auth } from 'aws-amplify';
@@ -14,9 +14,14 @@ class Navbar extends Component {
 	constructor(props){
 		// props and states
     	super(props);
+		this.state = {
+			show : false
+		};
     	// bind functions
 		//  this.displayUserAttributes 	=	this.displayUserAttributes	.bind(this);
 	    this.deleteUser				=	this.deleteUser.bind(this);
+		this.handleShow				= 	this.handleShow.bind(this);
+		this.handleClose			= 	this.handleClose.bind(this);
   	}
 
   	// async displayUserAttributes(){
@@ -24,6 +29,18 @@ class Navbar extends Component {
   	// 	var user = await Auth.currentAuthenticatedUser({ bypassCache: true });
   	// 	console.log(user.attributes);
   	// }
+
+	handleShow(){
+		this.setState({
+				show: true
+			});
+	}
+
+	handleClose(){
+		this.setState({
+				show: false
+			});
+	}
 
   	async deleteUser(){
 
@@ -52,6 +69,7 @@ class Navbar extends Component {
                 });
             }))
             .catch(this.onError);
+		this.handleClose();
   	}
 
 	render() {
@@ -113,12 +131,24 @@ class Navbar extends Component {
 	  				<Button
 	  					variant="secondary"
 	  					size="md"
-	  					onClick ={this.deleteUser}
+	  					onClick ={this.handleShow}
 	  					block>
 	    				Delete User
 	  				</Button>
 	  				<Postwrite username={this.props.username}></Postwrite>
 	    		</Jumbotron>
+			<Modal show={this.state.show} onHide={this.handleClose}>
+				<Modal.Header>
+					<Modal.Title>Confirm Account Deletion</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<p>Are you sure you want to delete your account? This cannot be undone!</p>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={this.handleClose}>Cancel</Button>
+					<Button variant="primary" onClick={this.deleteUser}>Confirm</Button>
+				</Modal.Footer>
+			</Modal>
 			</div>
     	);
   	}
