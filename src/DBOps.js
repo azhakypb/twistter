d47420d166928609734db9a4a5c5d6c6a5e3c38f
justin-u <junterr@purdue.edu>
2019-12-02 20:04:30 -0500

    update addTopics

--- src/DBOps.js
+++ src/DBOps.js
@@ -697,27 +697,13 @@ async function addTopics(author, topics) {
   var userData = await API.graphql(graphqlOperation(getUserTemplate, JSON.stringify({id: author})));
   console.log(userData);
   console.log(topics);
-  var userTopics = (userData.data.getUser.topics == null) ? "" : userData.data.getUser.topics;
-  var userTopicsArr = (userTopics.length == 0) ? [] : userTopics.split(",");
-  var userTopicsHash = [];
-  for (var i = 0; i < userTopicsArr; i++) {
-    userTopicsHash[userTopicsArr[i]] = true;
-  }
-  if (userTopics.length != 0) {
-    userTopics = userTopics + ",";
-  }
-  // add the new topics to user topics field
-  for (var i = 0; i < topics.length; i++) {
-    if (userTopicsHash[topics[i]] == true) {
-      continue; // skip if the topic already is in the topics field
-    }
-    else if (i == topics.length - 1) {
-      userTopics = userTopics + topics[i];
-    }
-    else {
-      userTopics = userTopics + topics[i] + ",";
+  var userTopics = (userData.data.getUser.topics == null) ? [] : userData.data.getUser.topics.split(",");
+  for(const topic of topics){
+    if(!userTopics.contains(topic)){
+      userTopics.push(topic);
     }
   }
+  userTopics = userTopics.join(',');
   // add new topics to each user
   var followers = userData.data.getUser.followers.items;
   for (var i = 0; i < followers.length; i++) {
